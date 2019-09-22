f77028931e4c067c9cadc8a75333645a42742780
HUGO <scf180015@163.com>
2019-09-22 19:32:03 +0800

    再次提交

--- Git_Test/src/com/itheima/Git_Test01.java
+++ Git_Test/src/com/itheima/Git_Test01.java
@@ -3,5 +3,10 @@ package com.itheima;
 public class Git_Test01 {
     public static void main(String[] args) {
         System.out.println("hello----------Git");
+        System.out.println("hello----------Git");
+        System.out.println("hello----------Git");
+        System.out.println("hello----------Git");
+        System.out.println("hello----------Git");
+
     }
 }
